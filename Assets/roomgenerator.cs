ca46da2e98409966fa6123411a98a6b0a636840c
ricoisdaman <enrico.wright@captini.com>
2024-01-16 23:13:25 +0000

    room generation init

--- Assets/roomgenerator.cs
+++ Assets/roomgenerator.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class RoomGenerator : MonoBehaviour
 {
@@ -8,35 +9,57 @@ public class RoomGenerator : MonoBehaviour
     public GameObject enemyPrefab;
     public GameObject player1Prefab;
     public GameObject player2Prefab;
-    public int width = 2;
-    public int height = 6;
-    public float roomWidth = 10f;
-    public float roomHeight = 10f;
+    public int roomCount = 12; // Total number of rooms to spawn
+    public float roomWidth = 10f; // Physical width of each room
+    public float roomHeight = 10f; // Physical height of each room
     public int totalEnemies = 10;
 
+    private HashSet<Vector2> occupiedPositions = new HashSet<Vector2>();
     private List<Transform> potentialEnemyPositions = new List<Transform>();
 
     void Start()
     {
-        GenerateRooms();
+        GenerateRandomRooms();
         PlaceEnemiesRandomly();
         SpawnPlayers();
     }
 
-    void GenerateRooms()
+    void GenerateRandomRooms()
     {
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                Vector3 roomPosition = new Vector3(x * roomWidth, y * roomHeight, 0);
-                GameObject room = Instantiate(roomPrefab, roomPosition, Quaternion.identity);
+        occupiedPositions.Clear();
+        Vector2 currentPos = Vector2.zero;
 
-                PlaceDoorsInRoom(room);
-                CollectEnemyPositions(room);
-            }
+        for (int i = 0; i < roomCount; i++)
+        {
+            Vector3 roomPosition = new Vector3(currentPos.x * roomWidth, currentPos.y * roomHeight, 0);
+            GameObject room = Instantiate(roomPrefab, roomPosition, Quaternion.identity);
+
+            occupiedPositions.Add(currentPos);
+
+            // Determine the next room position
+            currentPos = GetNextRoomPosition(currentPos);
+
+            PlaceDoorsInRoom(room);
+            CollectEnemyPositions(room);
         }
     }
+
+    Vector2 GetNextRoomPosition(Vector2 currentPos)
+    {
+        List<Vector2> possiblePositions = new List<Vector2>
+        {
+            currentPos + Vector2.up,    // Above
+            currentPos + Vector2.right, // Right
+            currentPos + Vector2.down,  // Below
+            currentPos + Vector2.left   // Left
+        };
+
+        possiblePositions.RemoveAll(pos => occupiedPositions.Contains(pos));
+        if (possiblePositions.Count == 0) return currentPos; // No more positions available
+
+        return possiblePositions[Random.Range(0, possiblePositions.Count)];
+    }
+
 
     void PlaceDoorsInRoom(GameObject room)
     {
@@ -76,11 +99,13 @@ public class RoomGenerator : MonoBehaviour
 
     void SpawnPlayers()
     {
-        if (width * height > 0)
+        if (occupiedPositions.Count > 0)
         {
-            Vector3 spawnRoomPosition = new Vector3(0, 0, 0);
+            Vector2 firstRoomPos = occupiedPositions.OrderBy(pos => pos.x).ThenBy(pos => pos.y).First();
+            Vector3 spawnRoomPosition = new Vector3(firstRoomPos.x * roomWidth, firstRoomPos.y * roomHeight, 0);
             Instantiate(player1Prefab, spawnRoomPosition, Quaternion.identity);
             Instantiate(player2Prefab, spawnRoomPosition + new Vector3(1, 0, 0), Quaternion.identity);
         }
     }
+
 }
